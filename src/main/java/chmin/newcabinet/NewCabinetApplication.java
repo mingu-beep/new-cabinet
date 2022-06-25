3429f2f10ce6b6510a97781475e3860d4b808179
mingu-beep <bramurve@gmail.com>
2022-06-25 23:02:52 +0900

    set primary value of project

---
+++ src/main/java/chmin/newcabinet/NewCabinetApplication.java
@@ -0,0 +1,13 @@
+package chmin.newcabinet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewCabinetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewCabinetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/chmin/newcabinet/NewCabinetApplicationTests.java
@@ -0,0 +1,13 @@
+package chmin.newcabinet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewCabinetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
